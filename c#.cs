6efbca8d50dfd8e0b7780f4060e8abc9ed5ba296
Poncho <jplyles1212@gmail.com>
2021-01-05 11:27:48 -0800

    Loop through IDs and print any that start with 'B'

--- c#.cs
+++ c#.cs
@@ -1,2 +1,9 @@
 string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
 
+foreach (string item in orderIDs)
+{
+    if(item.StartsWith("B"))
+    {
+      Console.WriteLine(item);
+    }
+}
