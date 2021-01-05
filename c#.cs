410aaf24631f8a19cdcbbe5669058713b83f73d4
Poncho <jplyles1212@gmail.com>
2021-01-05 10:44:21 -0800

    Create array of names to loop through and print each one

--- c#.cs
+++ c#.cs
@@ -1,16 +1,6 @@
-string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+string[] names = { "Bob", "Conrad", "Grant"};
 
-// fraudulentOrderIDs[0] = "A123";
-// fraudulentOrderIDs[1] = "B456";
-// fraudulentOrderIDs[2] = "C789";
-// fraudulentOrderIDs[3] = "D000";
-
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
-
-fraudulentOrderIDs[0] = "F000";
-
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent order to process.");
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
