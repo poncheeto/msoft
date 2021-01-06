217f441ac9464286352d80ac7f82c20b43ed3d4f
Poncho <jplyles1212@gmail.com>
2021-01-06 15:44:07 -0800

    Add whitespace to make code more readable

--- c#.cs
+++ c#.cs
@@ -1,22 +1,21 @@
-Random dice = new Random();
+// The purpose of this code is to reverse a string and count the number of times a particular character appears.
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
 
-int total = roll1 + roll2 + roll3;
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+string str = "The quick brown fox jumps over the lazy dog.";
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
-{
-    if ((roll1 == roll2) && (roll2 == roll3)) 
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6; 
-    } 
-    else 
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
+int x = 0;
+
+foreach (char i in charMessage) 
+{ 
+  if (i == 'o') 
+  { 
+    x++; 
+  } 
 }
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
