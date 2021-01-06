f3639c24cb8142208dcfff308f514e74db558745
Poncho <jplyles1212@gmail.com>
2021-01-06 15:38:41 -0800

    Add whitespace to appropriate lines of code for better readability

--- c#.cs
+++ c#.cs
@@ -1,11 +1,18 @@
-// Example 1:
-Console
+Random dice = new Random();
 
-.
-WriteLine
-("Hello World!"
-)
-;
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-//Example 2:
-string firstWord="Hello";string lastWord="World";Console.WriteLine(firstWord+" "+lastWord+"!");
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+    if ((roll1 == roll2) && (roll2 == roll3)) {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
+    } else {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
