77a24e48db8e59aa2d4487e72f545d4dcf0ea508
Poncho <jplyles1212@gmail.com>
2021-01-12 20:05:17 -0800

    Use ToString helper method to explicitly convert int to string

--- c#.cs
+++ c#.cs
@@ -1,5 +1,4 @@
-decimal myDecimal = 1.23456789m;
-float myFloat =(float)myDecimal;
-
-Console.WriteLine($"Decimal: {myDecimal}");
-Console.WriteLine($"Float: {myFloat}");
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
