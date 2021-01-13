b5c064881ddd929be9cd3f1c61a0097605e71757
Poncho <jplyles1212@gmail.com>
2021-01-12 19:55:59 -0800

    Perform cast from decimal to integral data type

--- c#.cs
+++ c#.cs
@@ -1,5 +1,5 @@
-int myInt = 3;
-Console.WriteLine($"int: {myInt}");
+decimal myDecimal = 3.14m;
+Console.WriteLine($"decmial: {myDecimal}");
 
-decimal myDecimal = myInt;
-Console.WriteLine($"decimal: {myDecimal}");
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
