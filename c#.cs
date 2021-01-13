e0689d7a023950e2d3d1b25ff21d270963e08d86
Poncho <jplyles1212@gmail.com>
2021-01-12 19:51:57 -0800

    Perform implicit conversion

--- c#.cs
+++ c#.cs
@@ -1,4 +1,5 @@
-int first = 2;
-string second = "4";
-string result = first + second;
-Console.WriteLine(result); 
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}");
