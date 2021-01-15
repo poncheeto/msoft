5bb0d458dbd4384717c014572ad2ebbbd2305583
Poncho <jplyles1212@gmail.com>
2021-01-14 18:24:00 -0800

    Change variable names

--- c#.cs
+++ c#.cs
@@ -1,10 +1,11 @@
 string[] values = { "12.3", "45", "ABC", "11", "DEF"};
 
 string message;
-float num = 0f;
+float sum = 0f;
 
 foreach (string value in values)
 {
+    float num;
     if(float.TryParse(value, out num))
     {
       Console.WriteLine(num);
