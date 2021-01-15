343cf5cf3c3710066cce921dfc8456b0a0eeaf9c
Poncho <jplyles1212@gmail.com>
2021-01-14 16:36:25 -0800

    Use TryParse to convert string value into int

--- c#.cs
+++ c#.cs
@@ -1,2 +1,10 @@
-string name = "Bob";
-Console.WriteLine(int.Parse(name));
+string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+  Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
