34a81eaa309aa3d5ada8746b5a908c8613cf6026
Poncho <jplyles1212@gmail.com>
2021-01-14 16:25:39 -0800

    Attempt to convert non-numeric string into int value

--- c#.cs
+++ c#.cs
@@ -1,5 +1,2 @@
-int value = (int)1.5m; // casting truncates
-Console.WriteLine(value);
-
-int value2 = Convert.ToInt32(1.5m); // converting rounds up
-Console.WriteLine(value2);
+string name = "Bob";
+Console.WriteLine(int.Parse(name));
