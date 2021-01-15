2bd3b98ef12462c50c80f54a3ae97bd08882aba2
Poncho <jplyles1212@gmail.com>
2021-01-14 16:58:43 -0800

    Iterate through array of string values

--- c#.cs
+++ c#.cs
@@ -1,15 +1,6 @@
-string value = "bad";
-int result = 0;
-if (int.TryParse(value, out result))
-{
-  Console.WriteLine($"Measurement: {result}");
-}
-else
-{
-    Console.WriteLine("Unable to report the measurement.");
-}
+string[] values = { "12.3", "45", "ABC", "11", "DEF"};
 
-// Since it is defined outside of the if statement,
-// it can be accessed later in your code.
-if (result > 0)
-  Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+foreach (string value in values)
+{
+    Console.WriteLine(value);
+}
