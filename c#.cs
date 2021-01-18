435cab1e970802f06eb7a059cfda8547428d1408
Poncho <jplyles1212@gmail.com>
2021-01-18 10:06:17 -0800

    Use ToLower method to indicate null value in element

--- c#.cs
+++ c#.cs
@@ -1,9 +1,12 @@
 string [] pallets = { "B14", "A11", "B12", "A13" };
 Console.WriteLine("");
 
-Console.WriteLine($"Before: {pallets[0]}");
+Console.WriteLine($"Before: {pallets[0].ToLower()}");
 Array.Clear(pallets, 0, 2);
-Console.WriteLine($"After: {pallets[0]}");
+if (pallets[0] != null)
+{
+  Console.WriteLine($"After: {pallets[0].ToLower()}");
+}
 
 Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
