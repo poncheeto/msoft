695933180404711822581b9ff2adf6d1b0d85ca3
Poncho <jplyles1212@gmail.com>
2021-01-18 09:21:58 -0800

    Practice sorting array using Sort method

--- c#.cs
+++ c#.cs
@@ -1,15 +1,8 @@
-int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+string [] pallets = { "B14", "A11", "B12", "A13" };
 
-// Your code here to set result1
-int  result1 = value1 / (int)value2;
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
-
-// Your code here to set result2
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-// Your code here to set result3
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
