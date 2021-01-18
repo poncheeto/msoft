6b21ac2b9500d5c0d96bd59d66fec95ea7609a02
Poncho <jplyles1212@gmail.com>
2021-01-18 09:54:49 -0800

    Print the first element of array before and after using Clear method

--- c#.cs
+++ c#.cs
@@ -1,7 +1,10 @@
 string [] pallets = { "B14", "A11", "B12", "A13" };
 Console.WriteLine("");
 
+Console.WriteLine($"Before: {pallets[0]}");
 Array.Clear(pallets, 0, 2);
+Console.WriteLine($"After: {pallets[0]}");
+
 Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
