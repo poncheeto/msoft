1591d0c1c1d7e08a8c691f53d7a13f239beb2682
Poncho <jplyles1212@gmail.com>
2021-01-18 09:05:00 -0800

    Practice casting

--- c#.cs
+++ c#.cs
@@ -1,20 +1,15 @@
-string[] values = { "12.3", "45", "ABC", "11", "DEF"};
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-string message = "";
-decimal total = 0.0m;
+// Your code here to set result1
+int  result1 = value1 / (int)value2;
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-foreach (string value in values)
-{
-    decimal num;
-    if(decimal.TryParse(value, out num))
-    {
-        total += num;
-    }
-    else
-    {
-        message += value;
-    }
-}
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+// Your code here to set result3
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
