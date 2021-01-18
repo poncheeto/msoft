2684ea6ebd45788d4ea38968a0506349d390c59f
Poncho <jplyles1212@gmail.com>
2021-01-18 09:50:40 -0800

    Clear the first two elements from array using Clear method

--- c#.cs
+++ c#.cs
@@ -1,11 +1,9 @@
 string [] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
 
-Console.WriteLine("Sorted...");
-Array.Sort(pallets);
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
-    Console.WriteLine($"-- {pallet}");
+  Console.WriteLine($"-- {pallet}");
 }
-
-Console.WriteLine("");
-Console.WriteLine("Reversed...");
